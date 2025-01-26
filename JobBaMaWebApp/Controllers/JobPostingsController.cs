c9e850c11ca41e0c5155b0f1ec5cc496049c62ef
Parsa <parsafathi.pf.2002@gmail.com>
2025-01-26 15:58:04 -0800

    تکمیل صفحه ایجاد آگهی استخدامی جدید

---
+++ JobBaMaWebApp/Controllers/JobPostingsController.cs
@@ -0,0 +1,52 @@
+﻿using JobBaMaWebApp.Models;
+using JobBaMaWebApp.Repositories;
+using JobBaMaWebApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobBaMaWebApp.Controllers
+{
+    public class JobPostingsController : Controller
+    {
+        private readonly IRepository<JobPosting> _repository;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public JobPostingsController(
+            IRepository<JobPosting> repository,
+            UserManager<IdentityUser> userManager)
+        {
+            _repository = repository;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var jobPostings = await _repository.GetAllAsync();
+            return View(jobPostings);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(JobPostingViewModel jobPostingViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var jobPosting = new JobPosting
+                {
+                    Title = jobPostingViewModel.Title,
+                    Description = jobPostingViewModel.Description,
+                    Company = jobPostingViewModel.Company,
+                    Location = jobPostingViewModel.Location,
+                    UserId = _userManager.GetUserId(User)
+                };
+                await _repository.AddAsync(jobPosting);
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(jobPostingViewModel);
+        }
+    }
+}
